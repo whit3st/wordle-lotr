bff6b92ab2b16ab72a8ad7e8831cb600bb1460ce
whit3st <alican15033@gmail.com>
2023-08-11 10:20:06 +0300

    better 404 page

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -3,6 +3,7 @@ export default function NotFound() {
     return (
         <div className="flex flex-col h-full gap-10">
             <h1 className="text-5xl mt-[100px] text-center font-bold">
+                There is nothing, <br />
                 Let&apos;s go back to Rivendell.
             </h1>
             <Link to="/" className="btn btn-secondary btn-sm text-xs md:btn-md border border-primary-content w-max self-end hover:-translate-x-2  font-semibold transition-all">← Rivendell</Link>
